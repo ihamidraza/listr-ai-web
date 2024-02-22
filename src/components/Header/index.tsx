55340f0fc1a061e9abf18a5fab473de394339241
Hamid Raza <hamid@simplusinnovation.com>
2024-02-22 23:17:51 +0500

    added style tweaks

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate, useLocation } from 'react-router-dom'
 import { Button, Img, Text, Line } from "components";
 
