2ecef4f0d9024ea5385aa56eeb8c3411b52873cf
Hamid Raza <hamid@simplusinnovation.com>
2024-05-31 20:41:59 +0500

    changes

--- apiConfig.js
+++ apiConfig.js
@@ -1,5 +1,6 @@
 // apiConfig.js
-export const BASE_URL = "https://648a-39-58-196-5.ngrok-free.app";
-// "http://51.20.123.36:3000"; // Replace with your server URL
+export const BASE_URL =
+  //  "https://648a-39-58-196-5.ngrok-free.app";
+  "http://51.20.123.36:3000"; // Replace with your server URL
 
 export const CONTEXT = "local";
